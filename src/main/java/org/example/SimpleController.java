8e7490726285960e1753c2d0b755f7d2bfa66383
Savira Saetang <www-best-123@hotmail.com>
2025-07-19 23:00:15 +0700

    initial project

---
+++ src/main/java/org/example/SimpleController.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot on Kubernetes!";
+    }
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
